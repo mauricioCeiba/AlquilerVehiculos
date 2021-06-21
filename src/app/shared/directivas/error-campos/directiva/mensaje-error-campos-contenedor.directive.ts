d0e1f25946c021e38ed08c4c6511d0cd7b640acb
Mauricio Collazos <mauricio.collazos@ceiba.com.co>
2021-06-21 16:34:39 -0500

    prueba

--- src/app/shared/directivas/error-campos/directiva/mensaje-error-campos-contenedor.directive.ts
+++
@@ -1,9 +0,0 @@
-import { Directive, ViewContainerRef } from '@angular/core';
-
-// tslint:disable:directive-selector
-@Directive({
-  selector: '[validar]',
-})
-export class MensajeErrorCamposContenedorDirective {
-  constructor(public vcr: ViewContainerRef) {}
-}
